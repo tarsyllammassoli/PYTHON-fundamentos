7ed9e80d190dbe8c28ccfc6ba271ba0d5c72d769
tarsyllammassoli <contatotaymassoli@gmail.com>
2026-08-13 19:58:25 -0300

    Trabalhando com DEF e agora DEF com Tuplas...

--- atv.py/atv_sistemas_com_def.py
+++ atv.py/atv_sistemas_com_def.py
@@ -121,6 +121,109 @@ def buscar_produto(produto5, nome):
             encontrado = True
             print(f"Produto: {produto['nome']}\nPreço: {produto['preco']}\nEstoque: {produto['estoque']}")
     if encontrado == False:
-        print(f"Produto não encontrado.")
+        print(f"Produto não encontrado. 1")
 
 buscar_produto(produtos5, "Mousepad")
+
+def atualizar_estoque(produtos5, nome, quantidade):
+    encontrado = False
+    for produto in produtos5:
+        if produto["nome"] == nome:
+            encontrado = True
+            produto["estoque"] += quantidade
+            print(f"O estoque foi atualizado para {produto["estoque"]}.")
+    if encontrado == False:
+        print(f"Produto não encontrado. 2")
+
+atualizar_estoque(produtos5, "Mouse", 45)
+
+def remover_estoque(produto5, nome, quantidade):
+    Encontrado = False
+    Estoque = False
+    for produto in produto5:
+        if produto["nome"] == nome:
+            Encontrado = True
+            if quantidade <= produto["estoque"]:
+                Estoque = True
+                produto["estoque"] -= quantidade
+                print(f'O estoque foi atualizado para {produto["estoque"]}.')
+    if Encontrado == False:
+        print(f'Produto não encontrado.')
+    elif Estoque == False:
+        print(f'Estoque insuficiente.')
+
+remover_estoque(produtos5, "Mouse", 200)
+
+alunos = [
+    {"nome": "Ana", "nota": 8},
+    {"nome": "Carlos", "nota": 5},
+    {"nome": "Julia", "nota": 9},
+    {"nome": "Pedro", "nota": 6},
+    {"nome": "Marina", "nota": 7}
+]
+
+def analisar_alunos(alunos):
+    aprovados = []
+    recuperacao = []
+    reprovados = []
+    for aluno in alunos:
+        if aluno['nota'] >= 7.0:
+            aprovados.append(aluno['nome'])
+        elif aluno['nota'] < 7.0 or aluno['nota'] > 5.0:
+            recuperacao.append(aluno['nome'])
+        else:
+            reprovados.append(aluno['nome'])
+
+    print(f'Aprovados: ')
+    for nome in aprovados:
+        print(nome)
+
+    print(f'Recuperação: ')
+    for nome in recuperacao:
+        print(nome)
+
+    print(f'Reprovados: ')
+    for nome in reprovados:
+        print(nome) 
+        
+    print(f'Total de Aprovados: {len(aprovados)}')
+
+analisar_alunos(alunos)
+
+produtos6 = [
+    {"nome": "Teclado", "preco": 150},
+    {"nome": "Mouse", "preco": 80},
+    {"nome": "Monitor", "preco": 900},
+    {"nome": "Headset", "preco": 250},
+    {"nome": "Webcam", "preco": 180}
+]
+
+def produtos_acima_De_200(produtos6):
+    produto_maior_200 = []
+    for produto in produtos6:
+        if produto['preco'] > 200:
+            produto_maior_200.append(produto['nome'])
+    return produto_maior_200
+
+resultado = produtos_acima_De_200(produtos6)
+print(resultado)
+
+categorias = ("Eletrônicos", "Informática", "Periféricos", "Acessórios")
+
+def mostrar_categorias(categorias):
+    tipo_c = 1
+    for categoria in categorias:
+        print(tipo_c, '-', categoria)
+        tipo_c += 1
+
+mostrar_categorias(categorias)
+
+theproduto = ("Notebook", 3500, 8)
+
+def mostrar_produto(theproduto):
+    nome, preco, estoque = theproduto
+    print('Nome: ', nome)
+    print('Preço: ', preco)
+    print('Estoque: ', estoque)
+
+mostrar_produto(theproduto)
